e0aed1f6af1703c56228806faed823cfdfb6fbdb
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-11-10 23:37:57 -0600

    API Project 2 - Phase 2 Complete :)

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,8 +3,25 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 42.0538387
-    @longitude = -87.67721
+    @address = params[:address]
+
+    @url_safe_address = "http://maps.googleapis.com/maps/api/geocode/json?address=" + URI.encode(@address) + "&sensor=false"
+
+
+    # Your code goes here.
+    url_2 = @url_safe_address
+    raw_data_2 = open(url_2).read
+    parsed_data_2 = JSON.parse(raw_data_2)
+    results = parsed_data_2["results"]
+    first = results[0]
+    geometry = first["geometry"]
+    location = geometry["location"]
+    @latitude = location["lat"]
+    @longitude = location["lng"]
+   
+
+    #@latitude = 42.0538387
+    #@longitude = -87.67721
     your_api_key = "dac657549d816cc1fba08b9a14656e65"
 
     # Your code goes here.
